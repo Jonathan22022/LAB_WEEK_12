5e648cede1da219474920aa74f245f977b9fb07c
Jonathan22022 <jonathangautama0406@gmail.com>
2025-12-01 10:23:15 +0700

    Commit No. 2 – Upgrading app with flow
    Commit No. 2 – Upgrading app with flow

--- app/build/tmp/kapt3/stubs/debug/com/example/test_lab_week_12/MovieRepository.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/test_lab_week_12/MovieRepository.java
@@ -1,15 +1,11 @@
 package com.example.test_lab_week_12;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\u0012\u001a\u00020\u0013H\u0086@\u00a2\u0006\u0002\u0010\u0014R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082D\u00a2\u0006\u0002\n\u0000R\u0017\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\u00060\b8F\u00a2\u0006\u0006\u001a\u0004\b\t\u0010\nR\u0014\u0010\u000b\u001a\b\u0012\u0004\u0012\u00020\u00060\fX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001a\u0010\r\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u000f0\u000e0\fX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\u0010\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u000f0\u000e0\b8F\u00a2\u0006\u0006\u001a\u0004\b\u0011\u0010\n\u00a8\u0006\u0015"}, d2 = {"Lcom/example/test_lab_week_12/MovieRepository;", "", "movieService", "Lcom/example/test_lab_week_12/api/MovieService;", "(Lcom/example/test_lab_week_12/api/MovieService;)V", "apiKey", "", "error", "Landroidx/lifecycle/LiveData;", "getError", "()Landroidx/lifecycle/LiveData;", "errorLiveData", "Landroidx/lifecycle/MutableLiveData;", "movieLiveData", "", "Lcom/example/test_lab_week_12/model/Movie;", "movies", "getMovies", "fetchMovies", "", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0012\u0010\u0007\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\n0\t0\bR\u000e\u0010\u0005\u001a\u00020\u0006X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000b"}, d2 = {"Lcom/example/test_lab_week_12/MovieRepository;", "", "movieService", "Lcom/example/test_lab_week_12/api/MovieService;", "(Lcom/example/test_lab_week_12/api/MovieService;)V", "apiKey", "", "fetchMovies", "Lkotlinx/coroutines/flow/Flow;", "", "Lcom/example/test_lab_week_12/model/Movie;", "app_debug"})
 public final class MovieRepository {
     @org.jetbrains.annotations.NotNull()
     private final com.example.test_lab_week_12.api.MovieService movieService = null;
     @org.jetbrains.annotations.NotNull()
     private final java.lang.String apiKey = "8a6981ca2124ec65ed3fcfcb057f67d2";
-    @org.jetbrains.annotations.NotNull()
-    private final androidx.lifecycle.MutableLiveData<java.util.List<com.example.test_lab_week_12.model.Movie>> movieLiveData = null;
-    @org.jetbrains.annotations.NotNull()
-    private final androidx.lifecycle.MutableLiveData<java.lang.String> errorLiveData = null;
     
     public MovieRepository(@org.jetbrains.annotations.NotNull()
     com.example.test_lab_week_12.api.MovieService movieService) {
@@ -17,18 +13,7 @@ public final class MovieRepository {
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final androidx.lifecycle.LiveData<java.util.List<com.example.test_lab_week_12.model.Movie>> getMovies() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.NotNull()
-    public final androidx.lifecycle.LiveData<java.lang.String> getError() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.Nullable()
-    public final java.lang.Object fetchMovies(@org.jetbrains.annotations.NotNull()
-    kotlin.coroutines.Continuation<? super kotlin.Unit> $completion) {
+    public final kotlinx.coroutines.flow.Flow<java.util.List<com.example.test_lab_week_12.model.Movie>> fetchMovies() {
         return null;
     }
 }
--- app/build/tmp/kapt3/stubs/debug/com/example/test_lab_week_12/MovieViewModel.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/test_lab_week_12/MovieViewModel.java
@@ -1,9 +1,17 @@
 package com.example.test_lab_week_12;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\b\u0010\u000e\u001a\u00020\u000fH\u0002R\u0017\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u00068F\u00a2\u0006\u0006\u001a\u0004\b\b\u0010\tR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\n\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\f0\u000b0\u00068F\u00a2\u0006\u0006\u001a\u0004\b\r\u0010\t\u00a8\u0006\u0010"}, d2 = {"Lcom/example/test_lab_week_12/MovieViewModel;", "Landroidx/lifecycle/ViewModel;", "movieRepository", "Lcom/example/test_lab_week_12/MovieRepository;", "(Lcom/example/test_lab_week_12/MovieRepository;)V", "error", "Landroidx/lifecycle/LiveData;", "", "getError", "()Landroidx/lifecycle/LiveData;", "popularMovies", "", "Lcom/example/test_lab_week_12/model/Movie;", "getPopularMovies", "fetchPopularMovies", "", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\b\u0010\u0011\u001a\u00020\u0012H\u0002R\u0014\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001a\u0010\b\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\n0\t0\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\u000b\u001a\b\u0012\u0004\u0012\u00020\u00070\f\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\u000eR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\u000f\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\n0\t0\f\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u000e\u00a8\u0006\u0013"}, d2 = {"Lcom/example/test_lab_week_12/MovieViewModel;", "Landroidx/lifecycle/ViewModel;", "movieRepository", "Lcom/example/test_lab_week_12/MovieRepository;", "(Lcom/example/test_lab_week_12/MovieRepository;)V", "_error", "Lkotlinx/coroutines/flow/MutableStateFlow;", "", "_popularMovies", "", "Lcom/example/test_lab_week_12/model/Movie;", "error", "Lkotlinx/coroutines/flow/StateFlow;", "getError", "()Lkotlinx/coroutines/flow/StateFlow;", "popularMovies", "getPopularMovies", "fetchPopularMovies", "", "app_debug"})
 public final class MovieViewModel extends androidx.lifecycle.ViewModel {
     @org.jetbrains.annotations.NotNull()
     private final com.example.test_lab_week_12.MovieRepository movieRepository = null;
+    @org.jetbrains.annotations.NotNull()
+    private final kotlinx.coroutines.flow.MutableStateFlow<java.util.List<com.example.test_lab_week_12.model.Movie>> _popularMovies = null;
+    @org.jetbrains.annotations.NotNull()
+    private final kotlinx.coroutines.flow.StateFlow<java.util.List<com.example.test_lab_week_12.model.Movie>> popularMovies = null;
+    @org.jetbrains.annotations.NotNull()
+    private final kotlinx.coroutines.flow.MutableStateFlow<java.lang.String> _error = null;
+    @org.jetbrains.annotations.NotNull()
+    private final kotlinx.coroutines.flow.StateFlow<java.lang.String> error = null;
     
     public MovieViewModel(@org.jetbrains.annotations.NotNull()
     com.example.test_lab_week_12.MovieRepository movieRepository) {
@@ -11,12 +19,12 @@ public final class MovieViewModel extends androidx.lifecycle.ViewModel {
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final androidx.lifecycle.LiveData<java.util.List<com.example.test_lab_week_12.model.Movie>> getPopularMovies() {
+    public final kotlinx.coroutines.flow.StateFlow<java.util.List<com.example.test_lab_week_12.model.Movie>> getPopularMovies() {
         return null;
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final androidx.lifecycle.LiveData<java.lang.String> getError() {
+    public final kotlinx.coroutines.flow.StateFlow<java.lang.String> getError() {
         return null;
     }
     
